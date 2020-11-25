c9ff42b6b1d1897253a1ed855a10e00d4d07ad86
unknown <hammad_205@hotmail.com>
2020-11-25 12:49:20 +0500

    add netlify identity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
-import React from "react"
+import React, { useEffect } from "react"
+import netlifyIdentity from 'netlify-identity-widget'
+import { Link } from "gatsby";
+
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  useEffect(() => {
+    netlifyIdentity.init({});
+  })
+  return <div>
+    <h1>Get Stuff Done!</h1>
+    <Link to='/'></Link>
+    <Link to='/app'></Link>
+    <button onClick={() => {
+      netlifyIdentity.open()
+    }}>Login</button>
+  </div>
 }
