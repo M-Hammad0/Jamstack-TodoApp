aa4daf07ef51ee7dc6eb34e8da8972c153fe4769
unknown <hammad_205@hotmail.com>
2020-11-28 04:09:58 +0500

    add signout btn

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import Navbar from 'react-bootstrap/Navbar'
+import { IdentityContext } from "../../identity-context"
+import Button from "react-bootstrap/Button"
+
 
 
 const Nav = () => {
+    const { identity: netlifyIdentity } = useContext(IdentityContext)
+
     return (
         <Navbar expand="lg" variant="light" bg="dark">
     <Navbar.Brand href="/">HOME</Navbar.Brand>
     <Navbar.Brand href="app">DASHBOARD</Navbar.Brand>
+    <Button size="sm" className="float-right" variant="secondary" onClick={() => {netlifyIdentity.open()}}>SignOut</Button>
   </Navbar>
     )
 }
